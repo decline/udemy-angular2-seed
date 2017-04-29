d7cdcc323e1be399c311bdbfcd5d0f8337e39e68
dominik.huppmann <dominik.huppmann@gmail.com>
2017-04-29 19:35:14 +0200

    add favorite component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {CoursesComponent} from "./courses.component";
 import {AuthorsComponent} from "./authors.component";
+import {FavoriteComponent} from "./favorite.component";
 
 @Component({
     selector: 'my-app',
@@ -8,7 +9,8 @@ import {AuthorsComponent} from "./authors.component";
         <h1>My First Angular 2 App</h1>
         <courses></courses>
         <authors></authors>
+        <favorite></favorite>
     `,
-    directives: [CoursesComponent, AuthorsComponent]
+    directives: [CoursesComponent, AuthorsComponent, FavoriteComponent]
 })
-export class AppComponent { }
+export class AppComponent {}
